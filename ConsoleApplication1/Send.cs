b838886dbf53a7a03d10e106116c035cd5a4c4ef
Gucio <gustaw@ohler.pl>
2018-05-21 16:33:51 +0200

    initial commit

---
+++ ConsoleApplication1/Send.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Collections.Generic;
+using RabbitMQ.Client.Events;
+using System.Collections.Concurrent;
+
+namespace ConsoleApplication1
+{
+    class Send
+    {
+
+        public static void pierwszaCzesc()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                string replyQueueName = "helloreply";
+                replyQueueName = channel.QueueDeclare().QueueName;
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                IBasicProperties properties = channel.CreateBasicProperties();
+                var correlationId = Guid.NewGuid().ToString();
+                properties.CorrelationId = correlationId;
+                properties.ReplyTo = replyQueueName;
+                BlockingCollection<string> respQueue = new BlockingCollection<string>();
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var response = Encoding.UTF8.GetString(body);
+                    if (ea.BasicProperties.CorrelationId == correlationId)
+                    {
+                        respQueue.Add(response);
+                    }
+                };
+
+                properties.Headers = new Dictionary<string, object>();
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i == 0)
+                        properties.Headers.Add("nr wiadomosci", i);
+                    else
+                    {
+                        properties.Headers.Remove("nr wiadomosci");
+                        properties.Headers.Add("nr wiadomosci", i);
+                    }
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                    channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: properties, body: body);
+                    channel.BasicConsume(consumer, replyQueueName, true);
+                    string resp = respQueue.Take();
+                    if (resp.Length > 0)
+                        Console.WriteLine("{0}", resp);
+                    else
+                        Console.WriteLine("No response");
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        public static void drugaCzesc()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs",
+                                        type: "topic");
+                string message = "";
+                var routingKey = "";
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i % 2 == 0)
+                        routingKey = "abc.def";
+                    else
+                        routingKey = "abc.xyz";
+                    message = "Hello world nr " + i;
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "topic_logs",
+                                         routingKey: routingKey,
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+                }
+            }
+        }
+        public static void Main()
+        {
+            pierwszaCzesc();
+            //drugaCzesc();
+        }
+    }
+}
---
+++ Odbiorca2/Receive.cs
@@ -0,0 +1,101 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Odbiorca
+{
+    class Receive
+    {
+        public static void pierwszaCzesc()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                var consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume(queue: "hello", autoAck: false, consumer: consumer);
+                Console.WriteLine(" [x] Awaiting RPC requests");
+                consumer.Received += (model, ea) =>
+                {
+                    string response = null;
+                    var body = ea.Body;
+                    var props = ea.BasicProperties;
+                    var replyProps = channel.CreateBasicProperties();
+                    replyProps.CorrelationId = props.CorrelationId;
+                    try
+                    {
+                        var message = Encoding.UTF8.GetString(body);
+                        int messageNumber = (int)ea.BasicProperties.Headers["nr wiadomosci"];
+                        messageNumber++;
+                        Console.WriteLine(" [x] Received {0} nr of message: {1}", message, messageNumber);
+                        response = "";
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(" [.] " + e.Message);
+                        response = "";
+                    }
+                    finally
+                    {
+                        var responseBytes = Encoding.UTF8.GetBytes(response);
+                        channel.BasicPublish(exchange: "", routingKey: props.ReplyTo, basicProperties: replyProps, body: responseBytes);
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    }
+
+                    Thread.Sleep(1000);
+                };
+                //channel.BasicConsume(queue: "hello", autoAck: false, consumer: consumer);
+
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        public static void drugaCzesc()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                string bindingKey = "*.xyz";
+
+                channel.QueueBind(queue: queueName,
+                                  exchange: "topic_logs",
+                                  routingKey: bindingKey);
+
+                Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                      routingKey,
+                                      message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+        public static void Main()
+        {
+            pierwszaCzesc();
+            //drugaCzesc();
+        }
+    }
+}
